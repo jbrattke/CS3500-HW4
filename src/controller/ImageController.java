f80ddbe1972172bd099848f586d230af03d3086e
brattke.j <brattke.j@northeastern.edu>
2022-06-07 00:31:27 -0400

    Revert "Revert "fixes + add red filter""
    This reverts commit e329b6e7415b953bdb61c86f21140c416bebc6f0.

--- src/controller/ImageController.java
+++ src/controller/ImageController.java
@@ -6,4 +6,7 @@ public interface ImageController {
    * This method runs the program. ??
    */
   void run(); //throws IllegalStateException, IllegalArgumentException
+
+
+  //should make a "write to readable" method in this interface
 }
