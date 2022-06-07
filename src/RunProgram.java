3bf15e6d2025d5e616e0e7af229729dacbec1007
brattke.j <brattke.j@northeastern.edu>
2022-06-06 20:14:08 -0400

    init commit.

--- src/RunProgram.java
+++ src/RunProgram.java
@@ -7,6 +7,12 @@ import controller.ImageControllerImpl;
  * A class used to run the image program in the command line.
  */
 public class RunProgram {
+
+  /**
+   * The main method, runs the image program.
+   *
+   * @param args the command line arguments
+   */
   public static void main(String[] args) {
     ImageController program = new ImageControllerImpl(new InputStreamReader(System.in));
     program.run();
